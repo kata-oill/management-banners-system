830c40311b17a358335c7d04e86b21eea2d14557
Katarzyna Grabowska <kati.grab@gmail.com>
2019-07-26 00:39:10 +0200

    Add and change classes in stylegiude.html and main_menu.scss for colapseFunction

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
+ 
+function colapseFunction() {
+  document.getElementById("dropdown-list").classList.toggle("show");
+}
+
  var dropdownContent = document.getElementById('dropdown-content');
 
- dropdownContent.addEventListener(click, function() {
+ dropdownContent.addEventListener('click', function() {
     dropdownContent.classList.remove('show');
+    console.log(2);
  });
 
  var dropDown = document.getElementById('nav-button');
 
  dropDown.addEventListener('click', function() {
     dropdownContent.classList.add('show');
+    console.log(1);
  });
