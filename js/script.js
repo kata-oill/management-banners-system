6221a561f16e5a581559aa1254d74b22f2c046bc
Katarzyna Grabowska <kati.grab@gmail.com>
2019-11-16 12:58:49 +0100

    Checking this is under a new branch as I'm not sure if the changes belong somewhere else

--- js/script.js
+++ js/script.js
@@ -1,18 +1,78 @@
  
-function colapseFunction() {
-  document.getElementById("dropdown-list").classList.toggle("show");
+function colapseFunctionWallet() {
+  document.getElementById("dropdown-list-wallet").classList.toggle("show");
 }
 
- var dropdownContent = document.getElementById('dropdown-content');
+ var dropdownContentWallet = document.getElementById('dropdown-list-wallet');
 
- dropdownContent.addEventListener('click', function() {
-    dropdownContent.classList.remove('show');
+ dropdownContentWallet.addEventListener('click', function() {
+    event.preventDefault();
+    dropdownContentWallet.classList.remove('show');
     console.log(2);
  });
 
- var dropDown = document.getElementById('nav-button');
+ var dropDownWallet = document.getElementById('nav-button-wallet');
 
- dropDown.addEventListener('click', function() {
-    dropdownContent.classList.add('show');
+ dropDownWallet.addEventListener('click', function() {
+    event.preventDefault();
+    dropdownContentWallet.classList.add('show');
+    dropdownContentNotification.classList.remove('show');
     console.log(1);
  });
+
+ function colapseFunctionNotification() {
+  document.getElementById("dropdown-list-notification").classList.toggle("show");
+}
+
+  var dropdownContentNotification = document.getElementById('dropdown-list-notification');
+
+ dropdownContentNotification.addEventListener('click', function() {
+    event.preventDefault();
+    dropdownContentNotification.classList.remove('show');
+    console.log(4);
+ });
+
+ var dropDownNotification = document.getElementById('nav-button-notification');
+
+ dropDownNotification.addEventListener('click', function() {
+    event.preventDefault();
+    dropdownContentNotification.classList.add('show');
+    dropdownContentWallet.classList.remove('show');
+    console.log(3);
+ });
+
+function sideOpenMenu() {
+    document.querySelector('.side-title').classList.add('show');
+    document.querySelector('.main-logo').classList.add('show');
+}
+
+var hamburger = document.getElementById('hamburger');
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+  event.preventDefault();
+  sideOpenMenu()
+  console.log(6);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
