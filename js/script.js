a9f02c8ffffa0a6852f45ebe4894ee25c53d98d3
Katarzyna Grabowska <kati.grab@gmail.com>
2019-11-17 14:57:53 +0100

    Fixed side menu closing

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function colapseFunctionWallet() {
 }
 
  var dropdownContentWallet = document.getElementById('dropdown-list-wallet');
+ var sideMenuOpenState = false;
 
  dropdownContentWallet.addEventListener('click', function() {
     event.preventDefault();
@@ -41,17 +42,27 @@ function colapseFunctionWallet() {
     console.log(3);
  });
 
-function sideOpenMenu() {
+function openSideMenu() {
     document.querySelector('.side-title').classList.add('show');
     document.querySelector('.main-logo').classList.add('show');
+    sideMenuOpenState = true;
+}
+
+function closeSideMenu() {
+    document.querySelector('.side-title').classList.remove('show');
+    document.querySelector('.main-logo').classList.remove('show');
+    sideMenuOpenState = false;
+}
+
+function toggleSideMenu() {
+  sideMenuOpenState? closeSideMenu() : openSideMenu(); 
 }
 
 var hamburger = document.getElementById('hamburger');
 
 document.querySelector('.hamburger').addEventListener('click', function() {
   event.preventDefault();
-  sideOpenMenu()
-  console.log(6);
+  toggleSideMenu();
 });
 
 
